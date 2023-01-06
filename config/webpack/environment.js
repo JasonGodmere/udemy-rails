403d86a33842df956ea4c6724efbbe63af4f638f
Jason Godmere <jgodmere808@gmail.com>
2023-01-06 01:35:23 +0000

    added boostrap and basic collapsable navbar

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require("webpack")
+
+// bootstrap dependencies
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+
 module.exports = environment
